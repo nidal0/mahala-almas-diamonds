d77e0847b1e5f703eb8efa1c92725845dfba366d
noodle <nidalabrar@hotmail.com>
2023-10-07 02:25:27 +0530

    diamond type, additional info and contact details sections

--- components/page-sections/form/Form.js
+++ components/page-sections/form/Form.js
@@ -21,7 +21,7 @@ import {
   Radio,
   RadioGroup,
   FormControl,
-  FormLabel,
+  Fade,
 } from "@mui/material";
 
 /* Styled Components */
@@ -34,12 +34,69 @@ const RootDiv = styled("div")(({ theme }) => ({
   width: "100%",
 }));
 
+const Container = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  height: "100%",
+}));
+
+const Column = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  alignItems: "center",
+}));
+
 const CustomPaper = styled(Paper)(({ theme }) => ({
   display: "flex",
-  flexDirection: "row",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  alignItems: "center",
+
+  width: "80%",
+  height: "90vh",
+}));
+
+const CustomFormControl = styled(FormControl)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
   justifyContent: "space-between",
   alignItems: "center",
   width: "100%",
+}));
+
+const SelectedImg = styled("img")(({ theme }) => ({
+  border: "4px solid #43A047",
+  borderRadius: "50%",
+  padding: "0.2rem",
+}));
+
+const DiamondTypeButtonRow = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "1rem",
+}));
+
+const DiamondTypeButton = styled(Button)(({ theme }) => ({
+  "&.MuiButton-outlined": {
+    background: "#FFFFFF",
+    color: "#5B7BB6",
+    border: "2px solid #5B7BB6",
+  },
+  "&.MuiButton-contained": {
+    background: "#5B7BB6",
+    color: "#FFFFFF",
+    border: "2px solid #5B7BB6",
+  },
+}));
+
+const ContactTextField = styled(TextField)(({ theme }) => ({
+  width: "40%",
 }));
 
 /* Copyright */
@@ -58,19 +115,48 @@ function Copyright() {
 }
 
 const steps = [
-  "Welcome",
-  "Select your location",
-  "Diamonds",
-  "Criteria",
-  "Additional Info",
+  "Location",
+  "Diamond Type",
+  "Shape",
+  "Carat",
+  "Color",
+  "Clarity",
+  "Cut",
+  "Certification",
+  "Additional Notes",
+  "Budget",
+  "Contact Details",
 ];
 
 export default function Form() {
+  /* Stepper States */
+
   const [activeStep, setActiveStep] = React.useState(0);
   const [completed, setCompleted] = React.useState({});
 
+  /* Input States */
+
   const [location, setLocation] = React.useState("");
   const [diamondType, setDiamondType] = React.useState("");
+  const [diamondShapes, setDiamondShapes] = React.useState([]);
+  const [diamondMinCarat, setDiamondMinCarat] = React.useState(0.1);
+  const [diamondMaxCarat, setDiamondMaxCarat] = React.useState(30);
+  const [diamondMinColor, setDiamondMinColor] = React.useState(0);
+  const [diamondMaxColor, setDiamondMaxColor] = React.useState(8);
+  const [diamondMinClarity, setDiamondMinClarity] = React.useState(0);
+  const [diamondMaxClarity, setDiamondMaxClarity] = React.useState(8);
+  const [diamondMinCut, setDiamondMinCut] = React.useState(0);
+  const [diamondMaxCut, setDiamondMaxCut] = React.useState(4);
+  const [diamondCertification, setDiamondCertification] = React.useState([]);
+  const [additionalNotes, setAdditionalNotes] = React.useState("");
+  const [diamondMinBudget, setDiamondMinBudget] = React.useState(0);
+  const [diamondMaxBudget, setDiamondMaxBudget] = React.useState(1000000);
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const [agree, setAgree] = React.useState(false);
+
+  /* Stepper Functions */
 
   const totalSteps = () => {
     return steps.length;
@@ -117,6 +203,15 @@ export default function Form() {
     setActiveStep(0);
     setCompleted({});
   };
+
+  /* Diamond Type useEffect */
+
+  React.useEffect(() => {
+    if (diamondType === "natural_grown" || diamondType === "lab_created") {
+      handleNext();
+    }
+  }, [diamondType]);
+
   return (
     <React.Fragment>
       <RootDiv>
@@ -136,9 +231,12 @@ export default function Form() {
             </Step>
           ))}
         </Stepper>
+
+        {/* Form Paper */}
+
         <CustomPaper
           variant="outlined"
-          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+          sx={{ my: { xs: 3, md: 3 }, p: { xs: 2, md: 3 } }}
         >
           {activeStep === steps.length ? (
             <React.Fragment>
@@ -156,179 +254,231 @@ export default function Form() {
               {/* Step 1: Enter Location */}
 
               {activeStep === 0 && (
-                <FormControl>
-                  <FormLabel id="select-location">
-                    Select your location
-                  </FormLabel>
-                  <RadioGroup
-                    aria-labelledby="select-location"
-                    name="select-location"
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
+                <Container>
+                  <Typography
+                    variant="h3"
+                    gutterBottom
+                    sx={{ margin: "0rem 0rem 3rem 0rem" }}
                   >
-                    <FormControlLabel
-                      value="india"
-                      control={<Radio />}
-                      label="India"
-                    />
-                    <FormControlLabel
-                      value="uae"
-                      control={<Radio />}
-                      label="UAE"
-                    />
-                    <FormControlLabel
-                      value="other"
-                      control={<Radio />}
-                      label="Other"
-                    />
-                    {location === "other" && (
+                    Select your location
+                  </Typography>
+                  <CustomFormControl>
+                    <RadioGroup
+                      row
+                      aria-labelledby="select-location"
+                      name="select-location"
+                      value={location}
+                      onChange={(e) => setLocation(e.target.value)}
+                    >
+                      <FormControlLabel
+                        value="india"
+                        labelPlacement="top"
+                        control={<Radio sx={{ display: "none" }} />}
+                        label={
+                          location === "india" ? (
+                            <SelectedImg
+                              src="/images/location/india.png"
+                              height={100}
+                              width={100}
+                              alt="India"
+                            />
+                          ) : (
+                            <img
+                              src="/images/location/india.png"
+                              height={100}
+                              width={100}
+                              alt="India"
+                            />
+                          )
+                        }
+                      />
+                      <FormControlLabel
+                        value="uae"
+                        labelPlacement="top"
+                        control={<Radio sx={{ display: "none" }} />}
+                        label={
+                          location === "uae" ? (
+                            <SelectedImg
+                              src="/images/location/uae.png"
+                              height={100}
+                              width={100}
+                              alt="UAE"
+                            />
+                          ) : (
+                            <img
+                              src="/images/location/uae.png"
+                              height={100}
+                              width={100}
+                              alt="UAE"
+                            />
+                          )
+                        }
+                      />
+                      <FormControlLabel
+                        value="other"
+                        labelPlacement="top"
+                        control={<Radio sx={{ display: "none" }} />}
+                        label={
+                          location === "other" ? (
+                            <SelectedImg
+                              src="/images/location/world.png"
+                              height={100}
+                              width={100}
+                              alt="Other"
+                            />
+                          ) : (
+                            <img
+                              src="/images/location/world.png"
+                              height={100}
+                              width={100}
+                              alt="Other"
+                            />
+                          )
+                        }
+                      />
+                    </RadioGroup>
+
+                    <Fade in={location === "other"}>
                       <TextField
+                        sx={{
+                          width: "30%",
+                          margin: "1rem 0rem 0rem 0rem",
+                          visibility:
+                            location === "other" ? "visible" : "hidden",
+                        }}
                         required
                         id="location"
                         name="location"
                         label="Enter location"
-                        fullWidth
                         variant="standard"
                         onChange={(e) => setLocation(e.target.value)}
                       />
-                    )}
-                  </RadioGroup>
-                </FormControl>
+                    </Fade>
+                  </CustomFormControl>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 3, ml: 1 }}
+                  >
+                    {"Next"}
+                  </Button>
+                </Container>
               )}
 
               {/* Step 2: Diamond Type */}
 
               {activeStep === 1 && (
-                <FormControl>
-                  <FormLabel id="select-diamond-type">
-                    So, what type of diamond are you looking for?
-                  </FormLabel>
-                  <RadioGroup
-                    aria-labelledby="select-diamond-type"
-                    defaultValue="natural_grown"
-                    name="select-diamond-type"
-                    value={diamondType}
-                    onChange={(e) => setDiamondType(e.target.value)}
+                <Container>
+                  <Typography
+                    variant="h3"
+                    gutterBottom
+                    sx={{ margin: "0rem 0rem 3rem 0rem" }}
                   >
-                    <FormControlLabel
-                      value="natural_grown"
-                      control={<Radio />}
-                      label="Natural Grown"
-                    />
-                    <FormControlLabel
-                      value="lab_created"
-                      control={<Radio />}
-                      label="Lab Created"
-                    />
-                  </RadioGroup>
-                </FormControl>
-              )}
-
-              {/* Step 3:  */}
-
-              {activeStep === 2 && (
-                <Grid container spacing={3}>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      required
-                      id="firstName"
-                      name="firstName"
-                      label="First name"
-                      fullWidth
-                      autoComplete="given-name"
-                      variant="standard"
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      required
-                      id="lastName"
-                      name="lastName"
-                      label="Last name"
-                      fullWidth
-                      autoComplete="family-name"
-                      variant="standard"
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      id="address1"
-                      name="address1"
-                      label="Address line 1"
-                      fullWidth
-                      autoComplete="shipping address-line1"
-                      variant="standard"
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      id="address2"
-                      name="address2"
-                      label="Address line zzzz"
-                      fullWidth
-                      autoComplete="shipping address-line2"
-                      variant="standard"
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      required
-                      id="city"
-                      name="city"
-                      label="City"
-                      fullWidth
-                      autoComplete="shipping address-level2"
-                      variant="standard"
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      id="state"
-                      name="state"
-                      label="State/Province/Region"
-                      fullWidth
-                      variant="standard"
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      required
-                      id="zip"
-                      name="zip"
-                      label="Zip / Postal code"
-                      fullWidth
-                      autoComplete="shipping postal-code"
-                      variant="standard"
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      required
-                      id="country"
-                      name="country"
-                      label="Country"
-                      fullWidth
-                      autoComplete="shipping country"
-                      variant="standard"
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          color="secondary"
-                          name="saveAddress"
-                          value="yes"
-                        />
+                    So, what type of diamond are you looking for?
+                  </Typography>
+                  <DiamondTypeButtonRow>
+                    <DiamondTypeButton
+                      disableElevation
+                      variant={
+                        diamondType === "natural_grown"
+                          ? "contained"
+                          : "outlined"
                       }
-                      label="Use this address for payment details"
-                    />
-                  </Grid>
-                </Grid>
+                      onClick={() => {
+                        setDiamondType("natural_grown");
+                        handleNext();
+                      }}
+                    >
+                      Natural Grown
+                    </DiamondTypeButton>
+
+                    <DiamondTypeButton
+                      disableElevation
+                      variant={
+                        diamondType === "lab_created" ? "contained" : "outlined"
+                      }
+                      onClick={() => {
+                        setDiamondType("lab_created");
+                        handleNext();
+                      }}
+                    >
+                      Lab Created
+                    </DiamondTypeButton>
+                  </DiamondTypeButtonRow>
+                </Container>
               )}
 
-              {/* Step 3 */}
+              {/* Step 9: Additional Notes */}
+
+              {activeStep === 8 && (
+                <Container>
+                  <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                      <TextField
+                        fullWidth
+                        id="notes"
+                        name="notes"
+                        label="Additional Notes"
+                        variant="outlined"
+                        multiline
+                        rows={6}
+                        value={additionalNotes}
+                        onChange={(e) => setAdditionalNotes(e.target.value)}
+                      />
+                    </Grid>
+                  </Grid>
+                </Container>
+              )}
+
+              {/* Step 11: Contact Details */}
+
+              {activeStep === 10 && (
+                <Container sx={{ gap: "2rem" }}>
+                  <ContactTextField
+                    required
+                    id="name"
+                    name="name"
+                    label="Name*"
+                    variant="standard"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+
+                  <ContactTextField
+                    id="email"
+                    name="email"
+                    label="Email"
+                    variant="standard"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+
+                  <ContactTextField
+                    id="phone"
+                    name="phone"
+                    label="Phone"
+                    variant="standard"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+
+                  <FormControlLabel
+                    sx={{ width: "40%" }}
+                    control={
+                      <Checkbox
+                        name="agree"
+                        checked={agree}
+                        onChange={() => {
+                          setAgree(!agree);
+                        }}
+                      />
+                    }
+                    label="I agree to share my details with Mahala Almas."
+                  />
+                </Container>
+              )}
+
+              {/* Buttons */}
 
               <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
                 {activeStep !== 0 && (
@@ -342,7 +492,7 @@ export default function Form() {
                   onClick={handleNext}
                   sx={{ mt: 3, ml: 1 }}
                 >
-                  {activeStep === steps.length - 1 ? "Place order" : "Next"}
+                  {activeStep === steps.length - 1 ? "Submit" : "Next"}
                 </Button>
               </Box>
             </React.Fragment>
